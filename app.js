3bc50492e9201b1225bd6a3aa8141fa4163c21f3
Nemanja Danev <nemanja.danev.93@gmail.com>
2020-03-22 19:24:09 +0100

    added dynamic data to the home page

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    let data = {
+        title: "We got the good stuff!",
+        subtitle: "Explore our showcased products below.."
+    };
+
+    res.render("index", { data: data });
 });
 
 app.get("/contact", (req, res) => {
