900b4287ce4e4886cc23098f60d408132ca52b45
Nemanja Danev <nemanja.danev.93@gmail.com>
2020-03-22 22:57:08 +0100

    added from on contact page

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+// const nodemailer = require("nodemailer");
 const app = express();
 
 // setup body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // setup template engine
 app.set("view engine", "ejs");
@@ -30,5 +32,45 @@ app.get("/contact", (req, res) => {
 
     res.render("contact", { data: data });
 });
+
+// Send Email
+// app.post("/send", (req, res) => {
+//     var output = `
+//         YOU HAVE A NEW CONTACT REQUEST!
+//         ---------------------------------------------------------
+//         Contact Details
+//         ---------------------
+//         First Name: ${req.body.firstname}
+//         Last Name: ${req.body.lastname}
+//         Email: ${req.body.email}
+//         Message:
+//         ${req.body.text}
+//     `;
+
+//     var transporter = nodemailer.createTransport({
+//         service: "gmail",
+//         auth: {
+//             user: "nem.dan.93@gmail.com",
+//             pass: "nemanja123"
+//         }
+//     });
+
+//     var mailOption = {
+//         from: req.body.email,
+//         to: "nemanja.danev.93@gmail.com",
+//         subject: req.body.subject,
+//         text: output
+//     };
+
+//     transporter.sendMail(mailOption, function(error, info) {
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             console.log("Email sent: " + info.response);
+//         }
+//     });
+
+//     res.redirect("/contact");
+// });
 
 app.listen(process.env.PORT || 3000);
